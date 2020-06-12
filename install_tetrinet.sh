43d81882df64ab3acba363d75868957070c4af9d
Jacob McDonald <jacob@mcawesome.org>
2020-06-12 09:05:40 -0500

    Fix binary download path for github.

--- install_tetrinet.sh
+++ install_tetrinet.sh
@@ -6,11 +6,12 @@ set -eo pipefail
 sudo dpkg --add-architecture i386
 sudo apt update
 sudo apt install --assume-yes wine32
-curl https://github.com/yottabit42/install_tetrinet/raw/master/tnet113.exe > tnet113.exe
+curl "https://raw.githubusercontent.com/yottabit42/install_tetrinet/master/tnet113.exe" -o tnet113.exe
 wine tnet113.exe
 echo "alias tetrinet='wine ~/.wine/drive_c/TetriNET/TETRINET.exe'" >> ~/.bashrc
 wine ~/.wine/drive_c/TetriNET/TETRINET.EXE
-clear
+echo
+echo --
 echo
 echo "In the future just type 'tetrinet' to run TetriNET."
 echo "(You will need to close and repoen this Terminal window once, before that works.)"
