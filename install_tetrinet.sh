1465cde937e114d539349d272e9b3a21a12d6160
Jacob McDonald <jacob@mcawesome.org>
2020-06-12 08:58:36 -0500

    Change installer pull location.
    The installer is now checked into this repo, and is called from the repo by the script. It has been checked for malware via virustotal.com. Since TetriNET is abandonware, it's safer to host a known-good version directly in the repo, rather than pulling from source.
    
    Also minor refactor to use bash command failure options directly, rather than chaining commands.

--- install_tetrinet.sh
+++ install_tetrinet.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
-#
+# Install wine x86, install TetriNET, add tetrinet alias.
 
-sudo dpkg --add-architecture i386 && \
-  sudo apt update && \
-  sudo apt install --assume-yes wine32 && \
-  curl http://tetrinet.us/tnet113.exe > tnet113.exe && \
-  wine tnet113.exe && \
-  echo "alias tetrinet='wine ~/.wine/drive_c/TetriNET/TETRINET.exe'" >> ~/.bashrc && \
-  wine ~/.wine/drive_c/TetriNET/TETRINET.EXE
+set -eo pipefail
+
+sudo dpkg --add-architecture i386
+sudo apt update
+sudo apt install --assume-yes wine32
+curl https://github.com/yottabit42/install_tetrinet/raw/master/tnet113.exe > tnet113.exe
+wine tnet113.exe
+echo "alias tetrinet='wine ~/.wine/drive_c/TetriNET/TETRINET.exe'" >> ~/.bashrc
+wine ~/.wine/drive_c/TetriNET/TETRINET.EXE
 clear
 echo
 echo "In the future just type 'tetrinet' to run TetriNET."
