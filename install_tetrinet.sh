2afdfe0522cc0277c5671a65d295e474c9767fda
Jacob McDonald <jacob@mcawesome.org>
2020-06-12 15:16:37 -0500

    Fixed curl command typos.

--- install_tetrinet.sh
+++ install_tetrinet.sh
@@ -8,9 +8,9 @@ sudo apt update
 sudo apt install --assume-yes wine32
 curl "https://raw.githubusercontent.com/yottabit42/install_tetrinet/master/tnet113.exe" -o tnet113.exe
 wine tnet113.exe
-curl "https://raw.githubusercontent.com/yottabit42/install_tetrinet/master/tetrinet-1.14.exe -o \
+curl "https://raw.githubusercontent.com/yottabit42/install_tetrinet/master/tetrinet-1.14.exe" -o \
   ~/.wine/drive_c/TetriNET/tetrinet-1.14.exe
-curl "https://raw.githubusercontent.com/yottabit42/install_tetrinet/master/tetrifast-1.14.exe -o \
+curl "https://raw.githubusercontent.com/yottabit42/install_tetrinet/master/tetrifast-1.14.exe" -o \
   ~/.wine/drive_c/TetriNET/tetrifast-1.14.exe
 echo "alias tetrinet='wine ~/.wine/drive_c/TetriNET/tetrifast-1.14.exe'" >> ~/.bashrc
 echo "alias tetrinet-big='sommelier -X --scale=0.8 wine ~/.wine/drive_c/TetriNET/tetrifast-1.14.exe'" >> ~/.bashrc
