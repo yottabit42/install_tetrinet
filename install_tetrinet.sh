aef030fc336050147b12a5511e8546416eddcc3d
Jacob McDonald <jacob@mcawesome.org>
2020-06-12 15:22:39 -0500

    Fix alias typos.

--- install_tetrinet.sh
+++ install_tetrinet.sh
@@ -14,8 +14,8 @@ curl "https://raw.githubusercontent.com/yottabit42/install_tetrinet/master/tetri
   ~/.wine/drive_c/TetriNET/tetrifast-1.14.exe
 echo "alias tetrinet='wine ~/.wine/drive_c/TetriNET/tetrifast-1.14.exe'" >> ~/.bashrc
 echo "alias tetrinet-big='sommelier -X --scale=0.8 wine ~/.wine/drive_c/TetriNET/tetrifast-1.14.exe'" >> ~/.bashrc
-echo "alias tetrinet-bigger='sommelier -X --scale=0.5 wine ~/.wine/drive_c/TetriNET/tetrifast.exe-1.14'" >> ~/.bashrc
-echo "alias tetrinet-biggest='sommelier -X --scale=0.3 wine ~/.wine/drive_c/TetriNET/tetrifast.exe-1.14'" >> ~/.bashrc
+echo "alias tetrinet-bigger='sommelier -X --scale=0.5 wine ~/.wine/drive_c/TetriNET/tetrifast-1.14.exe'" >> ~/.bashrc
+echo "alias tetrinet-biggest='sommelier -X --scale=0.3 wine ~/.wine/drive_c/TetriNET/tetrifast-1.14.exe'" >> ~/.bashrc
 wine ~/.wine/drive_c/TetriNET/tetrifast-1.14.exe
 echo
 echo --
