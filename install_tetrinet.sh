f4513c94f35254c175366134ae9c06b2c5c8c6cb
Jacob McDonald <jacob@mcawesome.org>
2020-06-14 17:36:47 -0500

    Optimize installation.
    Installation now uses a consolidated TetriFast 1.14 bundle.

--- install_tetrinet.sh
+++ install_tetrinet.sh
@@ -6,18 +6,14 @@ set -eo pipefail
 sudo dpkg --add-architecture i386
 sudo apt update
 sudo apt install --assume-yes wine32
-curl "https://raw.githubusercontent.com/yottabit42/install_tetrinet/master/tnet113.exe" -o tnet113.exe
-wine tnet113.exe
-curl "https://raw.githubusercontent.com/yottabit42/install_tetrinet/master/tetrifast-1.14.exe" -o \
-  ~/.wine/drive_c/TetriNET/tetrifast-1.14.exe
+curl "https://raw.githubusercontent.com/yottabit42/install_tetrinet/master/tetrifast114.tbz2" -o tetrifast114.tbz2
+tar xvjf tetrifast114.tbz2 -C ~/ --overwrite
 echo "alias tetrinet='wine ~/.wine/drive_c/TetriNET/tetrifast-1.14.exe'" >> ~/.bashrc
 echo "alias tetrinet-big='sommelier -X --scale=0.8 wine ~/.wine/drive_c/TetriNET/tetrifast-1.14.exe'" >> ~/.bashrc
 echo "alias tetrinet-bigger='sommelier -X --scale=0.5 wine ~/.wine/drive_c/TetriNET/tetrifast-1.14.exe'" >> ~/.bashrc
 echo "alias tetrinet-biggest='sommelier -X --scale=0.3 wine ~/.wine/drive_c/TetriNET/tetrifast-1.14.exe'" >> ~/.bashrc
 wine ~/.wine/drive_c/TetriNET/tetrifast-1.14.exe
-echo
-echo --
-echo
+clear
 echo "In the future just type 'tetrinet' to run TetriNET."
 echo "If TetriNET is too small in Chrome OS/Crostini, try 'tetrinet-big' or 'tetrinet-bigger' or 'tetrinet-biggest'."
 echo "(You will need to close and repoen this Terminal window once, before those commands work.)"
